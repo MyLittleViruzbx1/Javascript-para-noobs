2773708b11836f2231d7677caa1cfbab09d4c135
MyLittleViruzbx1 <mylittleviruz@gmail.com>
2024-11-15 11:59:19 -0600

    JSDocs

---
+++ app.js
@@ -0,0 +1,21 @@
+let a = 11;
+let b = a;
+
+let ana = 11;
+
+if(ana >= 18){
+    console.log('es mayor de edad');
+}else{
+    console.log('es menor de edad');
+}
+
+
+console.log('======================');
+(ana >= 18) ? console.log('ana es mayor') : console.log('ana es menor');
+
+
+
+
+
+
+
