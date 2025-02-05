98b20be4f24cbc01c266bc50f65b0ed93712f5a6
MyLittleViruzbx1 <mylittleviruz@gmail.com>
2025-02-04 18:28:29 -0600

    avances menores

---
+++ 01-Fundamentos/assets/js/primitivos-reservados.js
@@ -0,0 +1,21 @@
+let nombre = 'myz';
+
+console.log(nombre);
+
+let numero = 21;
+console.log(numero);
+console.log(typeof numero);
+
+let heroe = true;
+
+let arregloCosas = [
+    true, 
+    123,
+    'Fernando',
+    function(){},
+    ()=>{},
+    {a: 1},
+    ['X' , 'Megaman' , 'Zero' , 'Dr. Light']
+];
+
+console.log(arregloCosas[6][3]);
